12ee4221bd74e6837ae235931b64f74e5b704853
dinagasa <divyanagasai2014@gmail.com>
2023-06-17 17:08:33 +0530

    First commit

---
+++ src/main/java/com/example/SpringbootFinal6/SpringbootFinal6Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootFinal6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFinal6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFinal6Application.class, args);
+	}
+
+}
