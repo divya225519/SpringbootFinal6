832a49d3e09e74b6e7e89c3dddb9db7f8e0e4600
Divya <divyanagasai2014@gmail.com>
2023-06-19 08:54:42 +0530

    Nagendra Commit

--- src/main/java/com/example/SpringbootFinal6/SpringbootFinal6Application.java
+++ src/main/java/com/example/SpringbootFinal6/SpringbootFinal6Application.java
@@ -2,12 +2,19 @@ package com.example.SpringbootFinal6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;  
 
 @SpringBootApplication
-public class SpringbootFinal6Application {
+public class SpringbootFinal6Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootFinal6Application.class, args);
-	}
-
-}
+	@Override  
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)   
+	{  
+	return application.sources(SpringbootFinal6Application.class);  
+	}  
+	public static void main(String[] args)   
+	{  
+	SpringApplication.run(SpringbootFinal6Application.class, args);  
+	}  
+	}  
